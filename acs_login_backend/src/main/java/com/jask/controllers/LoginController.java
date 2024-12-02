072df00a4811c6e310d5709f92e992ca0f321bb5
Edward King <king8three@gmailcom>
2024-12-01 22:01:34 -0500

    back done

--- acs_login_backend/src/main/java/com/jask/controllers/LoginController.java
+++ acs_login_backend/src/main/java/com/jask/controllers/LoginController.java
@@ -1,13 +1,22 @@
 package com.jask.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jask.data.LoginRepository;
+import com.jask.models.LoginClass;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @RestController
@@ -18,8 +27,21 @@ public class LoginController {
     @Autowired LoginRepository loginRepository;
 
     @GetMapping
-    public String getMethodName(@RequestParam String param) {
-        return new String();
+    public List <LoginClass> getLogins() {
+        return loginRepository.findAll();
+    }
+
+    @PostMapping
+    public LoginClass createLogin(@RequestBody LoginClass login){
+        return loginRepository.save(login);
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<LoginClass> getUserById(@PathVariable int id) {
+        return loginRepository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
     
 }
