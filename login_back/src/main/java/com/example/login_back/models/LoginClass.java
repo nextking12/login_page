004ea195999b5c1053cd3be8a9b23aab5fef8e6f
nextking12 <king8three@gmail.com>
2024-12-14 15:45:25 -0500

    test

---
+++ login_back/src/main/java/com/example/login_back/models/LoginClass.java
@@ -0,0 +1,20 @@
+package com.example.login_back.models;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+
+@Entity
+@Data
+@Table
+public class LoginClass {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+}
